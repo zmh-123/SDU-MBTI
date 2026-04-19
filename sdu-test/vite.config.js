47b5ca4a5c713da59fbf1575ad868a6e8c89b472
menghao <1641003875@qq.com>
2026-04-19 19:08:39 +0800

    自动化

--- sdu-test/vite.config.js
+++ sdu-test/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  // 把这里的 sdu-test 换成你真实的 GitHub 仓库名称！
+  base: '/SDU-MBTI/',
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
 })
