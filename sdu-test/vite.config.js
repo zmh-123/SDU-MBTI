a584844c7106c6fa628a9def57b037e5bace8814
menghao <1641003875@qq.com>
2026-04-19 22:41:07 +0800

    移除相关文件

--- sdu-test/vite.config.js
+++ sdu-test/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // 把这里的 sdu-test 换成你真实的 GitHub 仓库名称！
   base: '/SDU-MBTI/',
   plugins: [
     vue(),
-    tailwindcss(),
   ],
 })
